348a55325149e51aff0bc33f3aea8e35357a9a16
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2025-10-20 23:23:38 +0200

    Exclude e2e tests to be run with vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,13 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./tests/setup/test-setup.ts'],
+    // Exclude e2e tests from Vitest
     exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
       '**/tests/e2e/**', // Exclude e2e tests
     ],
   },
